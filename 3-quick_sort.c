7474d10e814a73baaa9cf7aed7d32efaee83c1bf
EbubeCode <chukwuma258@gmail.com>
2022-07-19 03:06:41 +0100

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,73 @@
+#include "sort.h"
+
+/**
+ * partition - finds the partition position
+ * @array: the array
+ * @low: lower index
+ * @high: higher index
+ * @size: size of the array
+ *
+ * Return: the index of partition
+ */
+size_t partition(int *array, int low, int high, size_t size)
+{
+	int piv = array[high], temp;
+	int i = (low - 1), j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= piv)
+		{
+			i++;
+			temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+			if (j != i)
+				print_array(array, size);
+		}
+	}
+	j = i + 1;
+	temp = array[high];
+	array[high] = array[j];
+	array[j] = temp;
+	if (j != high)
+		print_array(array, size);
+
+	return (j);
+}
+
+/**
+ * q_sort - recursive funtion to sort the array
+ * @low: lower index
+ * @high: higher index
+ * @size: size of the array
+ * @array: the array
+ */
+void q_sort(int *array, int low, int high, size_t size)
+{
+	int piv;
+
+	if (low < high)
+	{
+		piv = partition(array, low, high, size);
+
+		q_sort(array, low, piv - 1, size);
+
+		q_sort(array, piv + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	q_sort(array, 0, size - 1, size);
+}
